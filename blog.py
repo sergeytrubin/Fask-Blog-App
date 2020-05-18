8aaaf091df7d5a3d5be9ed514278b159ae39eb4b
sergeytrubin <strubin@gmail.com>
2020-05-18 16:11:05 +0300

    Created html termplates and simple login function

---
+++ blog.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    post = db.Column(db.String(), nullable=False)
+
+
+migrate = Migrate(app, db)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    status = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+            request.form['password'] != app.config['PASSWORD']
+        error = 'Invalid Credentials. PLease try again.'
+        status_code = 401
+    else:
+        session['logged_in'] = True
+        return redirect(url_for('main'))
+    return render_template('login.html', error=error), status_code
+
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.173')
