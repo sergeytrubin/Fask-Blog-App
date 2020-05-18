8853f24ac975d5d836506ca1efefab365594fc92
sergeytrubin <strubin@gmail.com>
2020-05-18 18:19:40 +0300

    Added login function - Verifies the credentials with user in config

--- blog.py
+++ blog.py
@@ -21,16 +21,23 @@ migrate = Migrate(app, db)
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
-    status = 200
+    status_code = 200
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or \
-            request.form['password'] != app.config['PASSWORD']
-        error = 'Invalid Credentials. PLease try again.'
-        status_code = 401
-    else:
-        session['logged_in'] = True
-        return redirect(url_for('main'))
+                request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. PLease try again.'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
     return render_template('login.html', error=error), status_code
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were looged out')
+    return redirect(url_for('login'))
 
 
 @app.route('/main')
